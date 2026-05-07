e4844ba96ad8046414691a5b453d35dbb797b2f6
Logan Richan <121440901+argonchandelier@users.noreply.github.com>
2026-05-06 22:15:26 -0700

    Time: 1 ms (87.4%), Space: 19.3 MB (58.49%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -2,14 +2,12 @@ conv = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
 class Solution:
     def romanToInt(self, s: str) -> int:
-        n = len(s)
-        s += 'I'
         res = 0
-        for i in range(n):
+        for i in range(len(s)-1):
             n = conv[s[i]]
             if n >= conv[s[i+1]]:
                 res += n
                 continue
             res -= n
         
-        return res
+        return res+conv[s[-1]]
