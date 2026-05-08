0769c029d8ed977b0cedad8b18a40c1647974463
Logan Richan <121440901+argonchandelier@users.noreply.github.com>
2026-05-07 22:48:10 -0700

    Time: 0 ms (100%), Space: 19.5 MB (17.42%) - LeetHub

---
+++ 0068-text-justification/0068-text-justification.py
@@ -0,0 +1,47 @@
+class Solution:
+    def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
+        start = 0
+        nc, nw = 0, 0
+        lines = []
+        for i, word in enumerate(words):
+            l = len(word)
+            nca = l + (1 if nw > 0 else 0)
+            if maxWidth >= nc+nca:
+                nw += 1
+                nc += nca
+                continue
+
+            # overflowing; add line
+            if nw > 1:
+                nspaces, extra = divmod(maxWidth-nc+nw-1, nw-1)
+            else:
+                nspaces, extra = maxWidth-nc, 0
+            txt = [words[start]]
+            short = ''.join([" "]*nspaces)
+            lng = short+" " if extra > 0 else short
+            for j in range(1, nw):
+                k = start+j
+                if extra > 0:
+                    txt.append(lng)
+                    extra -= 1
+                else:
+                    txt.append(short)
+                txt.append(words[k])
+            if nw == 1:
+                txt.extend(short)
+            lines.append(''.join(txt))
+            start = i
+            nc, nw = l, 1
+
+        txt = [words[start]]
+        txtl = len(words[start])
+        for j in range(start+1, len(words)):
+            txt.append(" ")
+            txt.append(words[j])
+            txtl += 1+len(words[j])
+        txt.extend([" "]*(maxWidth - txtl))
+        lines.append(''.join(txt))
+
+        return lines
+            
+
