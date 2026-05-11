cff951949abaa5ef95163ef296528d3edafa6623
Logan Richan <121440901+argonchandelier@users.noreply.github.com>
2026-05-10 22:45:47 -0700

    Time: 23 ms (74.88%), Space: 19.9 MB (70.97%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
@@ -1,28 +1,36 @@
 class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
-        # Are all words unique? maybe make a hashmap from word to set
         m, mm, n = len(words), len(words[0]), len(s)
         mmm = m*mm
         mp = {}
+        #set0 = set()
         res = []
         for i, word in enumerate(words):
             if word in mp:
                 mp[word] += 1
                 continue
             mp[word] = 1
+            #set0.add(word[0])
         
-        for i in range(n-mmm+1):
+        for offset in range(mm):
             mpNew = {}
-            for j in range(i, i+mmm, mm):
+            count = 0
+            for j in range(offset, n-mm+1, mm):
                 ss = s[j:j+mm]
-                if ss not in mp:
-                    break
+                if ss not in mp: # ss[0] not in set0 or 
+                    mpNew = {}
+                    count = 0
+                    continue
+                count += 1
                 if ss in mpNew:
                     mpNew[ss] += 1
-                    continue
-                mpNew[ss] = 1
-            else:
+                else:
+                    mpNew[ss] = 1
                 if mpNew == mp:
-                    res.append(i)
+                    res.append(j-mmm+mm)
+                if count == m:
+                    count -= 1
+                    word = s[j-mmm+mm:j+2*mm-mmm]
+                    mpNew[word] -= 1
         
         return res
