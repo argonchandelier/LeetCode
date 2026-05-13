91443a6b4f1dc04d030ae52ce7bb7238b015668c
Logan Richan <121440901+argonchandelier@users.noreply.github.com>
2026-05-12 18:00:59 -0700

    Time: 0 ms (100%), Space: 19.3 MB (62.83%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.py
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -1,20 +1,16 @@
 class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
-        if len(nums) == 0:
-            return []
-        a = nums[0]
-        b = a
         res = []
-        cut = len(nums)-1
-        for i, num in enumerate(nums[1:], start=1):
-            if b+1 == num:
-                b += 1
-                continue
-            add = str(a)+"->"+str(b) if a != b else str(a)
-            res.append(add)
-            a = b = num
-        add = str(a)+"->"+str(b) if a != b else str(a)
-        res.append(add)
+        i = 0
+        while i < len(nums):
+            a = nums[i]
+            while i < len(nums)-1 and nums[i+1] == nums[i]+1:
+                i += 1
+            if nums[i] == a:
+                res.append(f"{a}")
+            else:
+                res.append(f"{a}->{nums[i]}")
+            i += 1
         
         return res
         
