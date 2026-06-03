585f74576d0595c8fadbbf9f2d6e37af019c0402
Logan Richan <121440901+argonchandelier@users.noreply.github.com>
2026-06-02 21:55:14 -0700

    Time: 20 ms (57.94%), Space: 19.4 MB (50.41%) - LeetHub

---
+++ 0945-snakes-and-ladders/0945-snakes-and-ladders.py
@@ -0,0 +1,47 @@
+from functools import lru_cache
+
+class Solution:
+    @lru_cache
+    def rc(self, ns):
+        r = self.n-1 - (ns // self.n)
+        c = ns % self.n if (self.n+r)&1 == 1 else self.n-1 - (ns % self.n)
+        return r, c
+
+    def snakesAndLadders(self, board: List[List[int]]) -> int:
+        self.n = n = len(board)
+        f = n**2
+        seen = [False]*f
+        seen[0] = True
+
+        spaces = [0]
+        rolls = 0
+        while spaces:
+            rolls += 1
+            nspaces = []
+            for space in spaces:
+                for add in range(1,7):
+                    nspace = space + add
+                    if nspace >= f-1:
+                        if nspace == f-1:
+                            return rolls
+                        break
+                    
+                    r, c = self.rc(nspace)
+                    val = board[r][c]
+                    if val == -1:
+                        if seen[nspace]:
+                            continue
+                        seen[nspace] = True
+                        nspaces.append(nspace)
+                        continue
+
+                    nspace2 = val-1
+                    if nspace2 == f-1:
+                        return rolls
+                    if seen[nspace2]:
+                        continue
+                    seen[nspace2] = True
+                    nspaces.append(nspace2)
+            spaces = nspaces
+
+        return -1
