ef7c3513c47886ad904d3b0ee5c3186b789e404d
Logan Richan <121440901+argonchandelier@users.noreply.github.com>
2026-06-13 20:43:23 -0700

    Time: 0 ms (100%), Space: 19.4 MB (85.48%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -2,7 +2,6 @@ from bisect import bisect_left as bl
 
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        n = len(nums)
         tails = nums[:1]
         for num in nums[1:]:
             i = bl(tails, num)
@@ -11,33 +10,3 @@ class Solution:
                 continue
             tails[i] = num
         return len(tails)
-
-
-
-
-        '''
-        ends = [nums[0]]
-        #lens = [1]
-        for i, num in enumerate(nums[1:]):
-            p1, p2 = 0, len(ends)-1
-            while p1 <= p2:
-                c = (p2+p1)//2
-                #print(f"{p1 = }, {p2 = }, {c = }")
-                C = ends[c]
-                if C < num:
-                    p1 = c+1
-                    continue
-                #if C >= num:
-                p2 = c-1
-            #ends = ends[:p1] + [num] + ends[p1:]
-            if p1 == len(ends):
-                ends.append(num)
-            else:
-                ends[p1] = num
-            
-            #newLen = max(lens[:p1]) + 1 if p1 > 0 else 1
-            #lens = lens[:p1] + [newLen] + lens[p1:]
-        #print(f"{ends = }, {lens = }")
-        
-        return len(ends) #max(lens)
-        '''
