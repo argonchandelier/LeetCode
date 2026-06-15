23fcb1096f9d3270f370da7bcca63379b5b17f7f
Logan Richan <121440901+argonchandelier@users.noreply.github.com>
2026-06-14 23:44:20 -0700

    Time: 87 ms (52.94%), Space: 33 MB (69.01%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.py
+++ 0221-maximal-square/0221-maximal-square.py
@@ -1,41 +1,17 @@
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
         m, n = len(matrix), len(matrix[0])
-        vertical = [[0]*n for _ in range(m)]
-        horizontal = [[0]*n for _ in range(m)]
         matrix = [[int(matrix[r][c]) for c in range(n)] for r in range(m)]
-        mx = 0
-        if matrix[0][0] == 1:
-            horizontal[0][0] = 1
-            vertical[0][0] = 1
-            mx = 1
-        for r in range(1, m):
-            if matrix[r][0] == 0:
-                continue
-            vertical[r][0] = vertical[r-1][0] + 1
-            horizontal[r][0] = 1
-            mx = 1
-        for c in range(1, n):
-            if matrix[0][c] == 0:
-                continue
-            horizontal[0][c] = horizontal[0][c-1] + 1
-            vertical[0][c] = 1
-            mx = 1
-
+        mx = int(any(x == 1 for x in matrix[0]) or any(row[0] == 1 for row in matrix))
+        
         for r in range(1, m):
             for c in range(1, n):
                 if matrix[r][c] == 0:
                     continue
                 lr, lc = r-1, c-1
-                lv, lh = vertical[lr][c], horizontal[r][lc]
-                vertical[r][c] = 1 + lv
-                horizontal[r][c] = 1 + lh
-                val = 1 + min(lv, lh, matrix[lr][lc])
+                val = 1 + min(matrix[lr][c], matrix[r][lc], matrix[lr][lc])
                 matrix[r][c] = val
                 if val > mx:
                     mx = val
-        print(horizontal)
-        print(vertical)
-        print(matrix)
         return mx*mx
 
